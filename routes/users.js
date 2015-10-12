c2b5d44f1016d64c3292b5d42c37d769b3dd50f3
jepebe <jpb@statoil.com>
2015-10-12 10:03:39 +0200

    Initial Commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({}, {}, function (e, docs) {
+        res.render('users', {
+            "userlist": docs
+        });
+    });
+});
+
+module.exports = router;
